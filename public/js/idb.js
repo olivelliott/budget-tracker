2ecb15d3c625a53449b054c036580c86795dca71
Olive <olivelliott48@gmail.com>
2022-08-21 20:08:34 -0400

    Debug | Test | Complete indexedDB offlime implementation

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,12 +9,50 @@ request.onupgradeneeded = function(e) {
 
 request.onsuccess = function(e) {
     db = e.target.result;
-
     if (navigator.onLine) {
-        // * <<>>
+        uploadBudgetItem();
     }
 };
 
 request.onerror = function(e) {
     console.log(e.target.errorCode)
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget_item');
+    budgetObjectStore.add(record);
+};
+
+function uploadBudgetItem() {
+    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget_item');
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse.message);
+                }
+                const transaction = db.transaction(['new_budget_item'], 'readwrite');
+                const budgetObjectStore = transaction.objectStore('new_budget_item');
+                budgetObjectStore.clear();
+
+                alert('All budget items have been successfully submitted!')
+            })
+            .catch(err => {console.log(err)});
+        }
+    };
+};
+
+window.addEventListener('online', uploadBudgetItem);
